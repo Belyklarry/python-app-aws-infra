cdb96a6f2e3cc091555c3a51ae9d6d138c363213
larrymoyoSE <larrymoyo.devops@gmail.com>
2022-11-21 14:55:43 +0200

    disabled karpenter

--- main.tf
+++ main.tf
@@ -124,7 +124,10 @@ module "kubernetes_addons" {
 
 
   enable_aws_load_balancer_controller  = true
+  enable_karpenter                     = false
   enable_amazon_eks_aws_ebs_csi_driver = true
+  enable_aws_for_fluentbit             = true
+  enable_metrics_server                = true
 }
 
 
